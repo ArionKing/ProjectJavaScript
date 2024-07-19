e8f63b6afeb230d2f3274fcc5ccfb4dddf6540df
Arion <arionking@hotmail.com>
2024-07-19 09:01:47 +0100

    started on book, functions

--- index.js
+++ index.js
@@ -47,3 +47,55 @@ function prevSlide() {
 showSlide(currentSlide); 
 
 setInterval(nextSlide, 3000); // Automatically advance slides every 3 seconds
+
+// 
+
+// document.getElementById("message").innerText = "Hello World!"; // this is creating HTML through only JavaScript.
+
+// 
+
+window.alert("Hello World!");  // this calls the log method of the console object to display the content specified in the ().
+
+// 
+
+console.log("Hello World!")
+
+// 
+
+const firstName = "Arion";
+const valueOfPi = 3.142;
+let isValid = true;
+let jsObject = console;
+let jsMethod = console.log;
+let jsSymbol = Symbol();
+let emptyVariable = null;
+let unusedVariable 
+
+console.log("firstName:" + typeof firstName);
+console.log("valueOfPi:" + typeof valueOfPi);
+console.log("isValid:" + typeof isValid);
+console.log("jsObject:" + typeof jsObject);
+console.log("jsMethod:" + typeof jsMehtod);
+console.log("jsSymbol:" + typeof jsSymbol);
+console.log("emptyVariable:" + typeof emptyVariable);
+console.log("unusedVariable:" + typeof unusedVariable);
+
+//
+
+function square(arg) {
+    return arg * arg ; 
+}
+
+function add(argOne, argTwo = 10 ) {
+    return argOne + argTwo ;
+}
+
+function squareAdd(arg) {
+    let result = square(arg)
+    return result + add(arg)
+}
+
+console.log("8 x 8:" + square(8));
+console.log("8 + 20:" + add(8, 20)); 
+console.log("8 + 10:" + add(8));
+console.log("(8 x 8) + (8 + 10):" + squareAdd(8)); 
